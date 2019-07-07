7f78c316d2977e5c245dc91269104e24fc487962
kerianne16 <kerianne116@gmail.com>
2019-07-06 22:05:26 -0400

    fixed

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # burger2
+In this assignment, you're going to Sequelize the `Burger` app you made last unit. We've split this exercise into three different tiers, all with different tasks and expectations. Finish whichever tier will provide you with the most reasonable challenge.
+
+### Before you Begin
+
+1. Create a new folder called `sequelizedBurger`
+2. Copy the contents of your completed assignment from last unit.
+   * Remove the older git connection with `rm -R .git`.
+   * Then connect this folder with a new github repo.
+3. Run `sequelize init` to create the necessary files.
+
+4. **Note**: If for any reason you don't have a finished assignment for last unit, no problem. Message the instructor, who will provide you will a skeleton file to work of for this unit.
---
+++ public/assets/js/burger.js
@@ -0,0 +1,59 @@
+
+$(function () {
+    $(".change-devour").on("click", function (event) {
+        var id = $(this).data("id");
+        var newdevour = $(this).data("newdevour");
+
+        var newdevourState = {
+            devoured: newdevour
+        };
+
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newdevourState
+        }).then(
+            function () {
+                console.log("changed devour to", newdevour);
+
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+
+        event.preventDefault();
+
+        var newburger = {
+            name: $("#ca").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newburger
+        }).then(
+            function () {
+                console.log("created new burger");
+
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete-burger").on("click", function (event) {
+        var id = $(this).data("id");
+
+
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("deleted burger", id);
+
+                location.reload();
+            }
+        );
+    });
+});
