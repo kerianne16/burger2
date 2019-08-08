1c5805a99523a69a1dcefef8ba6cf186066c1862
kerianne16 <kerianne116@gmail.com>
2019-08-08 12:50:03 -0400

    fixing routes

--- server.js
+++ server.js
@@ -1,31 +1,42 @@
+// Packages
 var express = require("express");
-
-var PORT = process.env.PORT || 8080;
-
-var app = express();
-
-app.use(express.static("public"));
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var path = require('path');
 var exphbs = require("express-handlebars");
 
+// Express
+var app = express();
+var port = process.env.PORT || 3000;
+
+// Routes
+var routes = require("./routes/burgerRoutes");
+
+// DB Models
+var db = require('./models');
+
+// Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// Method-override - Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-var routes = require("./controllers/burgers_Controllers.js");
+// Routes
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
 
-app.use(routes);
-
-/ 
-app.listen(PORT, function () {
-
-    console.log("Server listening on: http://localhost:" + PORT);
+// listen on port 3000
+db.sequelize.sync().then(function () {
+  app.listen(port, function (err) {
+    console.log('Listening on port ' + port);
+  });
 });
-
-
-//needs to be fixed
